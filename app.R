0732b7d152cb124eefed5a5178c199eedae2b378
Cole Arendt <cole.arendt@outlook.com>
2018-09-06 09:25:02 -0400

    clean up display with listviewer

--- app.R
+++ app.R
@@ -12,57 +12,56 @@ ui <- function(req) {fluidPage(
         
         mainPanel(
             h2("session$clientData"),
-            verbatimTextOutput("clientdataText"),
+            jsoneditOutput("clientdataText"),
             h2("session"),
-            verbatimTextOutput("sessionInfo"),
-            h2("Request Object"),
-            jsonedit(as.list(req)),
+            jsoneditOutput("sessionInfo"),
+            h2("UI req object"),
+            jsonedit(as.list(req)
+                     , mode = 'view'
+                     , modes = list('view'))
         )
     )
 )}
 
 server <- function(input, output, session) {
     
-    
+    clean_environ <- function(environ){
+        if (is.environment(environ)) {
+            lenv <- as.list(environ)
+            lenv <- lenv[which(!lapply(lenv, typeof) %in% c("environment"))]
+            return(lenv)
+        } else {
+            return(environ)
+        }
+    }
+  
     # Store in a convenience variable
     cdata <- session$clientData
     
     
-    output$sessionInfo <- renderText({
+    output$sessionInfo <- renderJsonedit({
         calt <- as.list(session)
+        
         
     #browser()
         calt_type <- lapply(calt, typeof)
-        calt_clean <- calt[which(!calt_type %in% c("environment", "closure"))]
+        calt_clean <- calt[which(!calt_type %in% c("closure"))]
+        calt_clean <- lapply(calt_clean, clean_environ)
         calt_class <- lapply(calt_clean, class)
         calt_clean_2 <- calt_clean[which(!calt_class %in% c("reactivevalues", "shinyoutput"))]
         calt_final <- calt_clean_2
         calt_names <- names(calt_final)
         
-        calt_req <- as.list(calt$request)
-        print(names(calt_req))
-        print(as.list(calt_req$HEADERS))
-        
-        calt_final <- as.list(calt_req)
         print(lapply(calt_final, typeof))
-        calt_final <- calt_final[which(as.character(lapply(calt_final, typeof)) == "character")]
-        calt_names <- names(calt_final)
         
-        
-        all_calt <- lapply(calt_names, function(name){
-            paste(name, as.character(calt_final[[name]]), sep = " = ")
-        })
-        paste(all_calt, collapse = "\n")
+       jsonedit(calt_final
+                , mode = 'view'
+                , modes = list('view'))
     })
     
     # Values from cdata returned as text
-    output$clientdataText <- renderText({
-        cnames <- names(cdata)
-        
-        allvalues <- lapply(cnames, function(name) {
-            paste(name, cdata[[name]], sep = " = ")
-        })
-        paste(allvalues, collapse = "\n")
+    output$clientdataText <- renderJsonedit({
+        jsonedit(as.list(cdata), mode = 'view', modes = list('view'))
     })
 }
 
